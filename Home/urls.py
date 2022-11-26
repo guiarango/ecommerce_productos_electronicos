1859168a8a98c0bcd6f215c7b3fda9b45dabb5b4
Franco Gidaszewski <francogidaszewski@gmail.com>
2022-11-25 21:30:16 -0300

    Cambio nombre de app

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Base.views import *
+from Home.views import *
 
 urlpatterns = [
     path('home/', home, name='home'),
--- proyecto_ecommerce_electronico/urls.py
+++ proyecto_ecommerce_electronico/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("eureka/", include("Base.urls"))
+    path("eureka/", include("Home.urls"))
 ]
