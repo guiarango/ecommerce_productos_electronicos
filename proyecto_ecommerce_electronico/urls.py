e7ef20ef24774b0cf9b257eac91b2909ccf18687
Franco Gidaszewski <francogidaszewski@gmail.com>
2022-11-27 15:24:56 -0300

    #

--- proyecto_ecommerce_electronico/urls.py
+++ proyecto_ecommerce_electronico/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("eureka/", include("Categorias.urls")),
+    path("", include("Categorias.urls")),
     path("", include("Home.urls"))
 ]
