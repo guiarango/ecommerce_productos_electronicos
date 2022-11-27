ea97100e1713f7e275f94064ead421e5523b7a80
guiarango <guiarango@hotmail.com>
2022-11-27 12:45:31 -0500

    Se generan los cambios dentro de la entidad de About junto con su CRUD

--- About/urls.py
+++ About/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from About.views import *
 
 urlpatterns = [
-    path('', aboutPage, name='home'),
+    path('', aboutPage, name='about-landing'),
+    path('crear_fundador/', crear_fundador, name='about-crear-fundador'),
+    path('listar_fundador/', listar_fundador, name='about-listar-fundador'),
+    path('editar_fundador/<id>', editar_fundador, name='about-editar-fundador'),
+    path('eliminar_fundador/<id>', eliminar_fundador, name='about-eliminar-fundador'),
 ]
+
