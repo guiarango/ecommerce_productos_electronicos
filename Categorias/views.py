a457667aa7eb70d7c5bc5533494d94f0407d6786
Franco Gidaszewski <francogidaszewski@gmail.com>
2022-12-06 13:05:52 -0300

    #

--- Categorias/views.py
+++ Categorias/views.py
@@ -4,5 +4,5 @@ from Categorias.models import *
 # Create your views here.
 
 def categorias(request):
-    categorias=CategoriaProd.objects.all()
+    categorias=TCategoria.objects.all()
     return render(request,'Categorias/categorias.html', {"categorias":categorias})
