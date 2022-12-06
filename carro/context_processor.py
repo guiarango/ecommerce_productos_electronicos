6ee22f6adb9e0610ece2f280d99def7a711fce62
Franco Gidaszewski <francogidaszewski@gmail.com>
2022-12-06 11:12:47 -0300

    Cambios en home y carro

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,8 +1,9 @@
 def importe_total_carro(request):
     total=0
     if request.user.is_authenticated:
-        for key , value in request.session["carro"].items():
-            total=total+(float(value["precio"])*value["cantidad"])
+        if 'carro' in request.session:
+            for key , value in request.session["carro"].items():
+                total=total+(float(value["precio"])*value["cantidad"])
     else:
         total="debes hacer login"
         
